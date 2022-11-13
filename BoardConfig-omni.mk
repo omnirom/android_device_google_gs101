4ee6ff1aec40b230d49b5907d31f011a14136567
maxwen <max.weninger@gmail.com>
2022-11-13 17:24:47 +0100

    WIP gs101: use options from BoardConfigVendor
    Change-Id: I0ea907450f8e9e9d5e2e5d37c39966cb3c6b881a

--- BoardConfig-omni.mk
+++ BoardConfig-omni.mk
@@ -1,15 +1,19 @@
-BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
-
-AB_OTA_PARTITIONS += \
+# defined in vendor/google_devices/oriole/proprietary/BoardConfigVendor.mk
+# TODO - AB_OTA_PARTITIONS contains all the other images too like abl.img etc
+# do we want to ship all of them in every ota?
+#AB_OTA_PARTITIONS += \
 	vendor \
-	vendor_dlkm \
-	vbmeta_vendor
+	vendor_dlkm
 
-BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
-BOARD_VENDOR_DLKMIMAGE_FILE_SYSTEM_TYPE := ext4
-TARGET_COPY_OUT_VENDOR_DLKM := vendor_dlkm
+#BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
+#BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
+#BOARD_VENDOR_DLKMIMAGE_FILE_SYSTEM_TYPE := ext4
+#TARGET_COPY_OUT_VENDOR_DLKM := vendor_dlkm
 
 # Enable chained vbmeta for vendor
+AB_OTA_PARTITIONS += \
+	vbmeta_vendor
+
 BOARD_AVB_VBMETA_VENDOR := vendor
 BOARD_AVB_VBMETA_VENDOR_KEY_PATH := external/avb/test/data/testkey_rsa2048.pem
 BOARD_AVB_VBMETA_VENDOR_ALGORITHM := SHA256_RSA2048
