22c0ccbde3cc32cd862dd5898fd023b481e249a1
maxwen <max.weninger@gmail.com>
2022-11-13 17:23:38 +0100

    gs101: use omni AB postinstall script
    Change-Id: Ibdcef72a9218260406bb3f89fa8e20ce3ddafeaf

--- device-omni.mk
+++ device-omni.mk
@@ -3,3 +3,13 @@ DEVICE_PACKAGE_OVERLAYS += device/google/gs101/overlay-omni
 # EUICC
 PRODUCT_PACKAGES += \
     EuiccSupportPixelOverlay
+
+# custom A/B postinstall
+PRODUCT_PACKAGES += \
+    omnipreopt_script
+
+AB_OTA_POSTINSTALL_CONFIG += \
+    RUN_POSTINSTALL_system=true \
+    POSTINSTALL_PATH_system=system/bin/omnipreopt_script \
+    FILESYSTEM_TYPE_system=ext4 \
+    POSTINSTALL_OPTIONAL_system=true
