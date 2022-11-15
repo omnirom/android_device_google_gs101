acf0cf4f0a39c541fb3afb42f65824f45e187755
maxwen <max.weninger@gmail.com>
2022-11-15 15:54:32 +0100

    gs101: move omni sepolicy include
    Change-Id: I0625aedd499d35260e0d4762e6ed34eea19edfd9

--- BoardConfig-omni.mk
+++ BoardConfig-omni.mk
@@ -1,14 +1,9 @@
 # defined in vendor/google_devices/oriole/proprietary/BoardConfigVendor.mk
-# TODO - AB_OTA_PARTITIONS contains all the other images too like abl.img etc
-# do we want to ship all of them in every ota?
-#AB_OTA_PARTITIONS += \
-	vendor \
-	vendor_dlkm
-
-#BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
-#BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
-#BOARD_VENDOR_DLKMIMAGE_FILE_SYSTEM_TYPE := ext4
-#TARGET_COPY_OUT_VENDOR_DLKM := vendor_dlkm
+# AB_OTA_PARTITIONS
+# BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
+# BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
+# BOARD_VENDOR_DLKMIMAGE_FILE_SYSTEM_TYPE := ext4
+# TARGET_COPY_OUT_VENDOR_DLKM := vendor_dlkm
 
 # Enable chained vbmeta for vendor
 AB_OTA_PARTITIONS += \
@@ -19,3 +14,5 @@ BOARD_AVB_VBMETA_VENDOR_KEY_PATH := external/avb/test/data/testkey_rsa2048.pem
 BOARD_AVB_VBMETA_VENDOR_ALGORITHM := SHA256_RSA2048
 BOARD_AVB_VBMETA_VENDOR_ROLLBACK_INDEX := $(PLATFORM_SECURITY_PATCH_TIMESTAMP)
 BOARD_AVB_VBMETA_VENDOR_ROLLBACK_INDEX_LOCATION := 3
+
+include vendor/omni/sepolicy/sepolicy.mk
