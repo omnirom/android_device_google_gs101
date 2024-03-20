3db4463ba8c60038a62b72f91384270b28a471d1
maxwen <max.weninger@gmail.com>
2024-03-20 01:51:39 +0100

    gs101: add ro.camerax.extensions.enabled=true
    Change-Id: Ie42da8db0012e9036156f1e66021e1c1fb352bc6

--- device-omni.mk
+++ device-omni.mk
@@ -5,6 +5,9 @@ PRODUCT_PRODUCT_PROPERTIES += \
     ro.vendor.camera.extensions.package=com.google.android.apps.camera.services \
     ro.vendor.camera.extensions.service=com.google.android.apps.camera.services.extensions.service.PixelExtensions
 
+PRODUCT_SYSTEM_PROPERTIES += \
+    ro.camerax.extensions.enabled=true
+
 # EUICC
 PRODUCT_PACKAGES += \
     EuiccSupportPixelOverlay
